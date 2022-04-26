75c56fec5dc569881c48a2c43461df974de00281
Mikhail Titov PC <89531276499nokia@gmail.com>
2022-04-26 14:37:19 +0300

    Записали все ссылки в link_offset.txt

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
@@ -5,28 +7,11 @@ from pprint import pprint
 HEADERS = {'accept': '*/*',
            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'}
-
-Site = 'https://www.bundestag.de'
 max_offset = 736 + 1
-Person = {}
 
-with open('Data/Offset_0.html', 'r', encoding='utf-8') as file:
-    Offset = file.read()
 
-soup = BeautifulSoup(Offset, 'lxml')
-
-all_people = soup.find_all(class_='col-xs-4 col-sm-3 col-md-2 bt-slide')
-for i in range(len(all_people)):
-    people_link = f"{Site}{all_people[i].find('a')['href']}"
-    people_name = all_people[i].find('div', class_='bt-teaser-person-text').find('h3').text
-
-    internet = requests.get(people_link, headers=HEADERS).text
-    soup2 = BeautifulSoup(internet, 'lxml')
-    r = soup2.find(class_='col-xs-12 col-md-4').find_all('li')
-
-    dic_person = {}
-    for i in r:
-        dic_person[i.text.strip()] = i.find('a')['href']
-
-    Person[people_name] = dic_person
-pprint(Person)
+n = 0
+for n in range(max_offset):
+    url = f'https://www.bundestag.de/ajax/filterlist/de/abgeordnete/862712-862712?limit=20&noFilterSet=true&offset={n}'
+    with open('Data/link_offset.txt', 'a', encoding='utf-8') as f:
+        f.write(f'{url}\n')
