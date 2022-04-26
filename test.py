6cacf9bf6577eb85b8b987144de1dd7d9e56547f
Mikhail Titov PC <89531276499nokia@gmail.com>
2022-04-26 18:08:08 +0300

    чтение json

--- test.py
+++ test.py
@@ -7,11 +7,19 @@ from pprint import pprint
 HEADERS = {'accept': '*/*',
            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'}
-max_offset = 736 + 1
+
+with open('Person2.json') as f:
+    man = json.load(f)
 
 
-n = 0
-for n in range(max_offset):
-    url = f'https://www.bundestag.de/ajax/filterlist/de/abgeordnete/862712-862712?limit=20&noFilterSet=true&offset={n}'
-    with open('Data/link_offset.txt', 'a', encoding='utf-8') as f:
-        f.write(f'{url}\n')
+new = {}
+# for i in range(len(links)):
+#     for n in range(len(links[i])):
+#         new[links[i][n].keys()] = links[i][n].v
+# for i in range(20):
+#     key_ = list(links[i].keys())
+#     val_ = list(links[i].values())
+#     for n in range(len(key_)):
+#         new[key_[n]] = val_[n]
+
+pprint(man)
