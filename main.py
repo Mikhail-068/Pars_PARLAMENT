b78e8d084052af737006a3d4e11c89267c28dae4
Mikhail Titov PC <89531276499nokia@gmail.com>
2022-04-26 10:34:04 +0300

    Вывели словарь 2 стр

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 HEADERS = {'accept': '*/*',
            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'}
 
-# html = 'https://www.bundestag.de/abgeordnete'
+Site = 'https://www.bundestag.de'
 max_offset = 736 + 1
 
 # n = 0
@@ -18,5 +19,23 @@ with open('Data/Offset_0.html', 'r', encoding='utf-8') as file:
     Offset = file.read()
 
 soup = BeautifulSoup(Offset, 'lxml')
-r = soup.find(class_='col-xs-4 col-sm-3 col-md-2 bt-slide').find(class_='bt-teaser-person-text').find('h3').text
-print(r)
+all_people = soup.find_all(class_='col-xs-4 col-sm-3 col-md-2 bt-slide')
+people_link = f"{Site}{all_people[0].find('a')['href']}"
+people_name = all_people[0].find('div', class_='bt-teaser-person-text').find('h3').text
+# print(people_name)
+# print(f"{Site}{people_link}")
+
+internet = requests.get(people_link, headers=HEADERS).text
+soup2 = BeautifulSoup(internet, 'lxml')
+r = soup2.find(class_='col-xs-12 col-md-4').find_all('li')
+
+
+dic_person = {}
+for i in r:
+    dic_person[i.text.strip()] = i.find('a')['href']
+
+pprint(dic_person)
+
+
+# for people in all_people:
+#     people.
