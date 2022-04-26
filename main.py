6cbd0cc1293fcf885dd16a20eca06202bcf1a779
Mikhail Titov PC <89531276499nokia@gmail.com>
2022-04-26 09:08:55 +0300

    Записали первый Offset_0

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 HEADERS = {'accept': '*/*',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'}
 
-html = 'https://www.bundestag.de/abgeordnete'
+# html = 'https://www.bundestag.de/abgeordnete'
+max_offset = 736 + 1
 
-n=200
-url_all =  f'https://www.bundestag.de/ajax/filterlist/de/abgeordnete/862712-862712?limit=20&noFilterSet=true&offset={n}'
-r = requests.get(url_all, headers=HEADERS)
-print(r)
+n = 0
+url_all = f'https://www.bundestag.de/ajax/filterlist/de/abgeordnete/862712-862712?limit=20&noFilterSet=true&offset={n}'
+html_ = requests.get(url_all, headers=HEADERS).text
+
+
+
+with open('Data/Offset_0.html', 'w', encoding='utf-8') as file:
+    file.write(html_)
