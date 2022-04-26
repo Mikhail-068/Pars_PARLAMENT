987d498ff1b3b5198a70971da8b8902604ea796b
Mikhail Titov PC <89531276499nokia@gmail.com>
2022-04-26 18:24:02 +0300

    Конечный результат

--- main.py
+++ main.py
@@ -17,43 +17,44 @@ max_offset = 736 + 1
 
 
 
-with open('Data/Offset_0.html', 'r', encoding='utf-8') as file:
-    Offset = file.read()
+with open('Data/link_offset.txt', 'r', encoding='utf-8') as f:
+    links = f.read()
+list_links = links.split('\n')[:-1]
+
 
 
 Person = {}
 list_pers = []
 c = 1
-for n in range(2):
-    Offset = f'https://www.bundestag.de/ajax/filterlist/de/abgeordnete/862712-862712?limit=20&noFilterSet=true&offset={n}'
-    r = requests.get(Offset, headers=HEADERS).text
+
+
+
+for link in range(0, 730, 20):
+    print(link)
+    r = requests.get(list_links[link], headers=HEADERS).text
     soup = BeautifulSoup(r, 'lxml')
 
     all_people = soup.find_all(class_='col-xs-4 col-sm-3 col-md-2 bt-slide')
 
-    for i in range(len(all_people)):
-        people_link = f"{Site}{all_people[i].find('a')['href']}"
-        people_name = all_people[i].find('div', class_='bt-teaser-person-text').find('h3').text
-        c+=1
+
+    for i,pers in enumerate(all_people):
+        people_link = f"{Site}{pers.find('a')['href']}"
+        people_name = pers.find('div', class_='bt-teaser-person-text').find('h3').text
+
 
         internet = requests.get(people_link, headers=HEADERS).text
         soup2 = BeautifulSoup(internet, 'lxml')
         r = soup2.find(class_='col-xs-12 col-md-4').find_all('li')
-
 
         dic_person = {}
 
         for i in r:
             dic_person[i.text.strip()] = i.find('a')['href']
         Person[people_name] = dic_person
-    list_pers.append(Person)
 
-    print(f'Готова {n} страница')
-    print(f'количество имен: {c}')
-    print(f'Person: {len(Person)}\n\n')
+
 with open('Person2.json', 'w', encoding='utf-8') as f:
-    json.dump(list_pers, f, indent=4, ensure_ascii=False)
+    json.dump(Person, f, indent=4, ensure_ascii=False)
 
 
-# for people in all_people:
-#     people.
+
